2338d938cb2265fdedcaf76d492a7e51a8bf503f
NguyenTuanEZS <nguyentuan@ezs.vn>
2020-09-22 16:53:48 +0700

    App Bloom Spa

---
+++ IOS/cser21_ios/Pods/Target Support Files/Pods-Bloomspa-BloomspaUITests/Pods-Bloomspa-BloomspaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bloomspa_BloomspaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Bloomspa_BloomspaUITests
+@end
---
+++ IOS/cser21_ios/Pods/Target Support Files/Pods-BloomspaTests/Pods-BloomspaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BloomspaTests : NSObject
+@end
+@implementation PodsDummy_Pods_BloomspaTests
+@end
